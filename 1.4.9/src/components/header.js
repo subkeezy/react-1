8a63ac95c4bc61d1a41eebf1025b01553292b1b4
subkeezy <102140976+subkeezy@users.noreply.github.com>
2024-01-08 15:37:24 +0700

    Add files via upload

---
+++ 1.4.9/src/components/header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import NewTaskForm from "./new-task-form";
+
+const Header = ({onItemAdded}) => {
+  return (
+    <header className="header">
+    <h1>todos</h1>
+    <NewTaskForm
+      onItemAdded={onItemAdded}/>
+  </header>
+  )
+}
+
+export default Header
---
+++ 1.4.9/src/components/task-list.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import Tasks from "./tasks";
+
+const TaskList = ({todos, onDeleted}) => {
+  const elements = todos.map(el => {
+    const { id, ...elProps} = el;
+    return (
+      <Tasks key={id} {...elProps}
+      onDeleted={() => onDeleted(id)}/>
+    )
+  })
+
+  return (
+    <section className="main" id="main">
+      <ul className="todo-list">
+        { elements }
+      </ul>
+    </section>
+  )
+}
+
+export default TaskList
