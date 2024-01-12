22fb62508bbaea6864b298aeeeb5a6bcf9bf97b0
subkeezy <102140976+subkeezy@users.noreply.github.com>
2024-01-12 21:09:41 +0700

    Delete finished 1.5.5 directory

--- finished 1.5.5/src/index.js
+++
@@ -1,8 +0,0 @@
-import ReactDOM from 'react-dom/client';
-
-import App from './components/app';
-
-import './index.css';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
